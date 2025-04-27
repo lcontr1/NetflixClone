a6ae69dc21400572de5ce55d8197191df10fa4fc
lcontr1 <contrerasliliana22@gmail.com>
2025-04-26 22:36:43 -0500

    initial commit

---
+++ backend/controllers/authController.js
@@ -0,0 +1,15 @@
+export async function signup (req, res) {
+    try {
+
+    } catch (error) {
+        
+    }
+}
+
+export async function login(req, res) {
+    res.send ('Login route');
+}
+
+export async function logout(req, res) {
+    res.send ('Logout route');
+}
---
+++ backend/server.js
@@ -0,0 +1,17 @@
+// const express = require('express') // commonjs
+//es module or esm
+import express from 'express';
+import authRoutes from './routes/authRoute.js';
+import { ENV_VARS } from './config/envVars.js';
+import { connectionDB } from './config/db.js';
+
+const app = express();
+const PORT = ENV_VARS.PORT
+
+app.use('/api/v1/auth', authRoutes);
+
+
+app.listen(PORT, () => {
+    console.log('Server started at http://localhost' + PORT);
+    connectionDB();
+});
